cb8ccb3be534274c5fd4fbd8147d1d79b49a7d2b
Waleed A. Afifi <waleedafifich@gmail.com>
2020-09-09 03:16:42 +0300

    Lap 3 doone

--- __tests__/input.test.js
+++ __tests__/input.test.js
@@ -1,32 +1,52 @@
 'use strict';
 
 const minimist = require('minimist');
+minimist.mockImplementationOnce(() => ({ add: '133313331333' }));
 
 jest.mock('minimist');
 
-minimist.mockImplementation(() => {
-  return {
-    add: 'New note will be added',
-  };
-});
-
 const Input = require('../lib/input');
+
+jest.spyOn(global.console, 'log');
+jest.spyOn(global.console, 'error');
+
 
 describe('Input Module', () => {
   it('addNote() the validation if the note type is String', () => {
     const inputObj = new Input();
     expect(inputObj.addNote('valid note')).toEqual('valid note');
+    expect(inputObj.addNote(545454)).toEqual('ERROR: this is not valid note !!');
   });
 
-  it('addNote() the validation if the note type is not String', () => {
+  it('list() the validation  if  return false', () => {
     const inputObj = new Input();
-    expect(inputObj.addNote(30)).toEqual('ERROR: this is not valid note !!');
+    expect(inputObj.list()).toBeFalsy();
+  });
+
+  it('delete() the validation if  return false ', () => {
+    const inputObj = new Input();
+    expect(inputObj.delete()).toBeFalsy();
+  });
+
+  it('delete() the validation if  return false ', () => {
+    minimist.mockImplementationOnce(() => ({ delete: '133313331333' }));
+    const inputObj = new Input();
+    inputObj.action = '-d';
+    inputObj.id = '133313331333';
+    expect(inputObj.delete()).toEqual('133313331333');
+  });
+
+  it('category() the validation if  returne false', () => {
+    const inputObj = new Input();
+    expect(inputObj.category('cat')).toEqual('cat');
+    inputObj.category('');
+    expect(console.log).toHaveBeenCalled();
   });
 
   it('If the command (add) and data (the note) were both valid', () => {
     const inputObj = new Input();
     inputObj.action = '--add';
-    inputObj.note = 'Waleed New note';
+    inputObj.note = 'hello';
     expect(inputObj.valid()).toBeTruthy();
   });
 
@@ -34,4 +54,5 @@ describe('Input Module', () => {
     const inputObj = new Input();
     expect(inputObj.valid()).toBeFalsy();
   });
+
 });
--- __tests__/notes.test.js
+++ __tests__/notes.test.js
@@ -1,26 +1,61 @@
 'use strict';
 
+require('@code-fellows/supergoose');
 const Note = require('../lib/notes');
 
 jest.spyOn(global.console, 'log');
 
 describe('Note Module', () => {
-  it('execute() does its work', () => {
-    const objOFNote = new Note({ payload: 'yes', action: '--a' });
-    objOFNote.execute({ payload: 'yes', action: '--a' });
-    expect(console.log).toHaveBeenCalled();
+
+  it('add() should save the note to database', () => {
+    let obj = { note: 'note', category: 'TEEST' };
+    const notes = new Note(obj);
+    return notes.add()
+      .then(record => {
+        Object.keys(obj).forEach(key => {
+          expect(record[key]).toEqual(obj[key]);
+        });
+      });
   });
 
-  it('add() does its work', () => {
-    const objOFNote = new Note({ action: '--a', error: 'ERROR: this is not valid arg' });
-    objOFNote.add({ action: '--a', error: 'ERROR: this is not valid arg' });
-    expect(console.log).toHaveBeenCalled();
+  it('rendre() the note from database', () => {
+    let obj = { note: 'note', category: 'TEEST' };
+    const notes = new Note(obj);
+    return notes.rendre()
+      .then(record => {
+        Object.keys(obj).forEach(key => {
+          expect(record[key]).toEqual(obj[key]);
+        });
+      });
   });
 
-  it('add() does its work', () => {
-    const objOFNote = new Note({ payload: 'yes', action: '--a', note: 'valid note' });
-    objOFNote.add({ payload: 'yes', action: '--a', note: 'valid note' });
-    expect(console.log).toHaveBeenCalled();
+  it('list() all note from database', () => {
+    let obj = { note: 'note', category: 'TEEST' };
+    const notes = new Note(obj);
+    return notes.list()
+      .then(record => {
+        Object.keys(obj).forEach(key => {
+          expect(record[key]).toEqual(obj[key]);
+        });
+      });
   });
+
+  // it('execute() does its work', () => {
+  //   const objOFNote = new Note({ payload: 'yes', action: '--a' });
+  //   objOFNote.execute({ payload: 'yes', action: '--a' });
+  //   expect(console.log).toHaveBeenCalled();
+  // });
+
+  // it('add() does its work', () => {
+  //   const objOFNote = new Note({ action: '--a', error: 'ERROR: this is not valid arg' });
+  //   objOFNote.add({ action: '--a', error: 'ERROR: this is not valid arg' });
+  //   expect(console.log).toHaveBeenCalled();
+  // });
+
+  // it('add() does its work', () => {
+  //   const objOFNote = new Note({ payload: 'yes', action: '--a', note: 'valid note' });
+  //   objOFNote.add({ payload: 'yes', action: '--a', note: 'valid note' });
+  //   expect(console.log).toHaveBeenCalled();
+  // });
 
 });
