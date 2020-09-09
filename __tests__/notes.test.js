7c8dacefc7a4a79acef4b4682bd78e8dee5bed97
Waleed A. Afifi <waleedafifich@gmail.com>
2020-09-09 14:13:22 +0300

    fix note test

--- __tests__/notes.test.js
+++ __tests__/notes.test.js
@@ -1,44 +1,67 @@
 'use strict';
 
 require('@code-fellows/supergoose');
+
 const Note = require('../lib/notes');
 
-jest.spyOn(global.console, 'log');
+// jest.spyOn(global.console, 'log');
 
-describe('Note Module', () => {
 
-  it('add() should save the note to database', () => {
-    let obj = { note: 'note', category: 'TEEST' };
-    const notes = new Note(obj);
-    return notes.add()
+describe('Note Model', ()=> {
+  it('can create() a new Note item ', ()=> {
+    let obj = {note: 'test Note 1', category: 'FRUIT', action: 'add'};
+    const note = new Note(obj);
+
+    return note.add(obj)
       .then(record => {
         Object.keys(obj).forEach(key => {
+          console.log("Keys: ", record);
           expect(record[key]).toEqual(obj[key]);
         });
       });
+  }) ;
+
+  it('can list() a Note item()', ()=> {
+    let obj = {note: 'test Note 1', category: 'FRUIT', action: 'add'};
+    const note = new Note(obj);
+    return note.add(obj)
+      .then(record => {
+        return note.list(obj)
+          .then(noteItem => {
+            Object.keys(obj).forEach(key=> {
+              expect(noteItem[0][key]).toEqual(obj[key]);
+            });
+          });
+      });
+
   });
 
-  it('rendre() the note from database', () => {
-    let obj = { note: 'note', category: 'TEEST' };
-    const notes = new Note(obj);
-    return notes.rendre()
-      .then(record => {
-        Object.keys(obj).forEach(key => {
-          expect(record[key]).toEqual(obj[key]);
-        });
-      });
-  });
+});
 
-  it('list() all note from database', () => {
-    let obj = { note: 'note', category: 'TEEST' };
-    const notes = new Note(obj);
-    return notes.list()
-      .then(record => {
-        Object.keys(obj).forEach(key => {
-          expect(record[key]).toEqual(obj[key]);
-        });
-      });
-  });
+
+// describe('Note Module', () => {
+
+//   it('add() should save the note to database', () => {
+//     let obj = { note: 'note', category: 'TEEST' };
+//     const notes = new Note(obj);
+//     return notes.add()
+//       .then(record => {
+//         Object.keys(obj).forEach(key => {
+//           expect(key).toEqual(key);
+//         });
+//       });
+//   });
+
+//   it('list() all note from database', () => {
+//     let obj = { note: 'note', category: 'TEEST' };
+//     const notes = new Note(obj);
+//     return notes.list(obj)
+//       .then(record => {
+//         Object.keys(obj).forEach(key => {
+//           expect(key).toEqual(key);
+//         });
+//       });
+//   });
 
   // it('execute() does its work', () => {
   //   const objOFNote = new Note({ payload: 'yes', action: '--a' });
@@ -58,4 +81,4 @@ describe('Note Module', () => {
   //   expect(console.log).toHaveBeenCalled();
   // });
 
-});
+// });
